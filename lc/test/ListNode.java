27ef41c7edf1a843d037ffcbbae89b40c39c2a49
Yufei-Yan <yanyufei11111@163.com>
2020-03-04 00:53:34 -0600

    Add classes for converting string to singly linked list.

---
+++ lc/test/ListNode.java
@@ -0,0 +1,25 @@
+/*
+ * @Author: Yufei Yan (yanyufei11111@163.com) 
+ * @Date: 2020-03-03 17:07:27 
+ * @Last Modified by: Yufei Yan
+ * @Last Modified time: 2020-03-03 17:12:36
+ */
+package lc.test;
+
+/**
+ * Definition for singly-linked list.
+ * Adopted from leetcode.
+ * 
+ * @author Yufei Yan
+ * @version 0.0.1
+ */
+public class ListNode {
+  public int val;
+  public ListNode next;
+  
+  /**
+   * Constructor.
+   * @param x input integer value
+   */
+  public ListNode(int x) { val = x; }
+}
---
+++ lc/test/ToSinglyList.java
@@ -0,0 +1,43 @@
+/*
+ * @Author: Yufei Yan (yanyufei11111@163.com) 
+ * @Date: 2020-03-03 17:11:15 
+ * @Last Modified by: Yufei Yan
+ * @Last Modified time: 2020-03-03 17:22:22
+ */
+package lc.test;
+
+public class ToSinglyList {
+
+  /**
+   * Convert the input string to a singly integer list.
+   * @param input the input string.
+   * @return the head of the list.
+   */
+  public ListNode intList(String input) {
+    if (null == input) throw new IllegalArgumentException();
+
+    String[] segments = input.substring(1, input.length() - 1).split(",");
+
+    ListNode head = new ListNode(Integer.parseInt(segments[0].trim()));
+    ListNode temp = head;
+    for (int i = 1; i < segments.length; ++i) {
+      temp.next = new ListNode(Integer.parseInt(segments[i].trim()));
+      temp = temp.next;
+    }
+
+    return head;
+  }
+
+  public static void main(String[] args) {
+    String input = "[1, 2, 3]";
+
+    ToSinglyList tsl = new ToSinglyList();
+    ListNode head = tsl.intList(input);
+    ListNode temp = head;
+    while (temp != null) {
+      System.out.print(temp.val + " ");
+      temp = temp.next;
+    }
+    System.out.println();
+  }
+}
